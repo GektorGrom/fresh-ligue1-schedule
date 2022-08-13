67cd4ddba4af72163b2020ee4a2e3dde96c95c86
Slava Mednikov <gektorgrom@gmail.com>
2022-08-12 21:51:47 -0600

    Add dynamoDB data fetch

--- routes/[name].tsx
+++ routes/[name].tsx
@@ -1,7 +1,53 @@
 /** @jsx h */
-import { h } from "preact";
-import { PageProps } from "$fresh/server.ts";
+import {h} from "preact";
+import {Handlers, PageProps} from "$fresh/server.ts";
+import {
+  DynamoDBClient,
+  GetItemCommand,
+} from "https://esm.sh/@aws-sdk/client-dynamodb";
+import {
+  unmarshall,
+} from "https://esm.sh/@aws-sdk/util-dynamodb";
+
+const client = new DynamoDBClient({
+  region: Deno.env.get("AWS_REGION"),
+  credentials: {
+    accessKeyId: Deno.env.get("AWS_ACCESS_KEY_ID"),
+    secretAccessKey: Deno.env.get("AWS_SECRET_ACCESS_KEY"),
+  },
+})
+
+interface Match {
+  "id": string;
+  "away": string;
+  "chanel": string;
+  "competition": string;
+  "end": number;
+  "home": string;
+  "isLigueShow": boolean | string;
+  "isLive": boolean | string;
+  "start": number;
+  "title": string;
+}
+
+export const handler: Handlers<Match> = {
+  async GET(_, ctx) {
+    const { Item } = await client.send(new GetItemCommand({
+      TableName: "BeIN_schedule",
+      Key: {
+        id: {
+          S: ctx.params.name,
+        }
+      }
+    }));
+    return ctx.render(unmarshall(Item));
+  }
+}
 
 export default function Greet(props: PageProps) {
-  return <div>Hello {props.params.name}</div>;
+  return <div>
+    Hello {props.params.name}
+    <p>{props.data.away}</p>
+    <p>{props.data.name}</p>
+  </div>;
 }
--- routes/index.tsx
+++ routes/index.tsx
@@ -1,21 +1,17 @@
 /** @jsx h */
 import { h } from "preact";
-import { tw } from "@twind";
 import Counter from "../islands/Counter.tsx";
 
+import { Handlers } from "$fresh/server.ts";
+
+export const handler: Handlers = {
+    GET(req, ctx) {
+        console.log(import.meta.url);
+        return Response.redirect(req.url + 'foo')
+    },
+};
+
+
 export default function Home() {
-  return (
-    <div class={tw`p-4 mx-auto max-w-screen-md`}>
-      <img
-        src="/logo.svg"
-        height="100px"
-        alt="the fresh logo: a sliced lemon dripping with juice"
-      />
-      <p class={tw`my-6`}>
-        Welcome to `fresh`. Try updating this message in the ./routes/index.tsx
-        file, and refresh.
-      </p>
-      <Counter start={3} />
-    </div>
-  );
+  return (<div></div>);
 }
